eb50d3940bd2f386af7091cf3034f489d75a37ab
Randy618 <zhenying214@qq.com>
2018-08-15 17:34:25 +0800

    button

---
+++ src/components/button/Button.vue
@@ -0,0 +1,38 @@
+<template>
+    <button type="button" :class="classes"><slot></slot></button>
+    <button type="button" class="btn btn-default">Default</button>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-blue">Blue</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+</template>
+
+<script>
+    const prefixCls = 'btn';
+    export default {
+        name: "Button",
+        computed: {
+            classes () {
+                return [
+                    `${prefixCls}`,
+                    `${prefixCls}-${this.type}`,
+                    {
+                        [`${prefixCls}-long`]: this.long,
+                        [`${prefixCls}-${this.shape}`]: !!this.shape,
+                        [`${prefixCls}-${this.size}`]: this.size !== 'default',
+                        [`${prefixCls}-loading`]: this.loading != null && this.loading,
+                        [`${prefixCls}-icon-only`]: !this.showSlot && (!!this.icon || !!this.customIcon || this.loading),
+                        [`${prefixCls}-ghost`]: this.ghost
+                    }
+                ];
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
